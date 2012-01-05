cb5006e2948f1fe02802a64b36ce04c84090ccef
feng <18602871035@163.com>
2012-01-05 14:51:25 +0800

    all the values encode by base64

--- redis-dump.lua
+++ redis-dump.lua
@@ -5,68 +5,65 @@
 ------------------------------------------------------------------
 
 require 'redis'
-
-local seri_string = function (str)
-	return ("%q"):format(self);
-end
+require 'base64'
 
 --The table like array in C
 local seri_itable = function(t)
-	local res = "{";
-    for k, v in pairs(t) do 
-    	res = ('%s"%s",'):format(res, v)
+local res = "{";
+    for k, v in pairs(t) do
+     res = ('%s"%s",'):format(res, encode(v))
     end
 
-	res = ('%s}'):format(res); 
-	return res;
+res = ('%s}'):format(res);
+return res;
 end
 
 --The table is hash Table
 local seri_stable = function(t)
-	local res = "{";
-	for k,v in pairs(t)do
-    	res = ('%s"%s",'):format(res, k)
-    	res = ('%s"%s",'):format(res, v)
-	end
-		
-	res = ('%s}'):format(res); 
-	return res;
+local res = "{";
+for k,v in pairs(t)do
+     res = ('%s"%s",'):format(res, k)
+     res = ('%s"%s",'):format(res, encode(v))
+end
+
+res = ('%s}'):format(res);
+return res;
 end
 
 --The table's element is also a table ,and both are array like C
 local seri_itable2 = function(t)
-	local res = "{";
-    for k, v in pairs(t) do 
-    	for k1,v1 in pairs(v) do
-    		res = ('%s%s,'):format(res, v1)
-		end
+local res = "{";
+    for k, v in pairs(t) do
+     for k1,v1 in pairs(v) do
+     res = ('%s"%s",'):format(res, encode(v1))
+end
     end
 
-	res = ('%s}'):format(res); 
-	return res;
+res = ('%s}'):format(res);
+return res;
 end
 
 
 print("+++++++++++++++++++++++++++++++");
-print("++++++++  DUMP DATA start ...++");
+print("++++++++ DUMP DATA start ...++");
 print("+++++++++++++++++++++++++++++++");
 
--- init database and dump file 
-local ip   = arg[1] or '127.0.0.1';
+-- init database and dump file
+local ip = arg[1] or '127.0.0.1';
 local port = arg[2] or 6379;
-local which= arg[3] or 0; 
+local which= arg[3] or 0;
 local fileName = arg[4] or 'dump.rds'
 
 -- Make sure not loose data
 local dumpFile = io.open(fileName,"r");
-if nil ~= dumpFile then 
-	print("The dump file ["..fileName.."] is exist, rewrite it ? Y:N")
-	local usrChar = io.read();
-	if usrChar ~= "Y" and usrChar ~= "y" then
-  		dumpFile:close(); 
-		return;
-	end
-  dumpFile:close(); 
+if nil ~= dumpFile then
+print("The dump file ["..fileName.."] is exist, rewrite it ? Y:N")
+local usrChar = io.read();
+if usrChar ~= "Y" and usrChar ~= "y" then
+   dumpFile:close();
+return;
+end
+  dumpFile:close();
 end
 
 local dumpFile = io.open(fileName,"w");
@@ -81,51 +78,51 @@ local count = 1;
 local keys = redis:keys('*');
 for i,key in pairs(keys) do
     io.write(".");
-	if count >= 20 then
-		io.write("\n");
-		count = 1;
-	end			
-	
-    dumpFile:write("Entry {\n");
-	
-	dumpFile:write("key=");
-	dumpFile:write(("%q,\n"):format(key));
-	
-	local valueType = redis:type(key);
-	local value;
-	
-	if valueType == "string" then
-	 	value = redis:get(key);
-	elseif valueType == "set" then
-		value = redis:smembers(key);
-		value = seri_itable(value);
-	elseif valueType == "zset" then
-		value = redis:zrange(key,0,-1,'withscores');
-		value = seri_itable2(value);
-    elseif valueType == "list" then
-		value = redis:lrange(key,0,-1);   
-		value = seri_itable(value);
-	elseif valueType == "hash" then
-		value = redis:hgetall(key);
-		value = seri_stable(value);
-	end
-	
-
-	dumpFile:write("type=");
-	dumpFile:write(("%q,\n"):format(valueType));
-
-	dumpFile:write("value=");
-	dumpFile:write(("%q,\n"):format(value));
-
-	dumpFile:write("}\n\n");
-
-	count = count + 1;
+if count >= 20 then
+io.write("\n");
+count = 1;
 end
-	
+
+    dumpFile:write("Entry {\n");
+
+dumpFile:write("key=");
+dumpFile:write(("%q,\n"):format(key));
+
+local valueType = redis:type(key);
+local value;
+
+if valueType == "string" then
+value = encode(redis:get(key));
+elseif valueType == "set" then
+value = redis:smembers(key);
+value = seri_itable(value);
+elseif valueType == "zset" then
+value = redis:zrange(key,0,-1,'withscores');
+value = seri_itable2(value);
+    elseif valueType == "list" then
+value = redis:lrange(key,0,-1);
+value = seri_itable(value);
+elseif valueType == "hash" then
+value = redis:hgetall(key);
+value = seri_stable(value);
+end
+
+
+dumpFile:write("type=");
+dumpFile:write(("%q,\n"):format(valueType));
+
+dumpFile:write("value=");
+dumpFile:write(("%q,\n"):format(value));
+
+dumpFile:write("}\n\n");
+
+count = count + 1;
+end
+
 dumpFile:close();
 redis:quit();
 
 print("");
 print("+++++++++++++++++++++++++++++++");
-print("++++++++  DUMP DATA SUCCESS  ++");
+print("++++++++ DUMP DATA SUCCESS ++");
 print("+++++++++++++++++++++++++++++++");
--- redis-import.lua
+++ redis-import.lua
@@ -1,18 +1,20 @@
 #!/usr/bin/env lua
 --------------------------------------------------------------------
--- usage : ./redis-import.lua dataFile [ip] [port] [whichDB] 
--- function: import the lua data file dumped by redis-dump.lua 
+-- usage : ./redis-import.lua dataFile [ip] [port] [whichDB]
+-- function: import the lua data file dumped by redis-dump.lua
 -- to the redis DB.
 ------------------------------------------------------------------
 
 require 'redis'
+require 'lglib'
+require 'base64'
 
 local redis = nil;
 local count = 1;
 
 ------------------------------------------------------------------------
 -- 将序列化的字符串加载到内存中，生成lua对象
--- @param self  被处理字符串
+-- @param self 被处理字符串
 -- @return lua对象
 ------------------------------------------------------------------------
 local unseri = function (self)
@@ -27,87 +29,88 @@ local unseri = function (self)
 end
 
 function import_set(key,t)
-	for k,v in ipairs(t) do
-		redis:sadd(key,v);
-	end
+    for k,v in ipairs(t) do
+        redis:sadd(key,decode(v));
+    end
 end
 
 function import_list(key,t)
-	for k,v in ipairs(t) do
-		redis:rpush(key,v);
-	end
+    for k,v in ipairs(t) do
+        redis:rpush(key,decode(v));
+    end
 end
 
 function import_zset(key,t)
-	for i=1,#t,2 do
-		redis:zadd(key,t[i+1],t[i]);	
-	end
+    for i=1,#t,2 do
+        print(t[1], t[2])
+        redis:zadd(key,decode(t[i+1]),decode(t[i]));
+    end
 end
 
 function import_hash(key,t)
-	for i=1,#t,2 do
-		redis:hset(key,t[i],t[i+1]);	
-	end
+    for i=1,#t,2 do
+        redis:hset(key,t[i],decode(t[i+1]));
+    end
 end
 
 function import(key,type,value)
-	if type == "string" then
-    	redis:set(key,value); 
-	else
-		local t = unseri(value);
-
-		if type == "set" then
-			import_set(key,t);
-		elseif type == "zset" then
-			import_zset(key,t);
-		elseif type == "hash"  then
-			import_hash(key,t);
-		elseif type == "list"  then
-			import_list(key,t);
-		else 
-			print "[ERROR:] UNKNOWN TYPE";
-	    	return;
-		end
-	end	
+    if type == "string" then
+        redis:set(key,decode(value));
+    else
+    
+        local t = unseri(value);
+        if type == "set" then
+            import_set(key,t);
+        elseif type == "zset" then
+            import_zset(key,t);
+        elseif type == "hash" then
+            import_hash(key,t);
+        elseif type == "list" then
+            import_list(key,t);
+        else
+            print "[ERROR:] UNKNOWN TYPE";
+            return;
+        end
+    end
 end
 
 function Entry(self)
-	local key  = "";
-	local type = "";
-	local value= "";
+    local key = "";
+    local type = "";
+    local value= "";
 
-	io.write(".");
-	if count >= 20 then
-		io.write("\n");
-		count = 1;
-	end
-	count = count + 1;
-  
-	for k,v in pairs(self) do
-	--	print(k,v)
-		if k == "key" then
-			key = v;
-		elseif k == "type" then
-			type = v;
-		elseif k == "value" then
-			value = v;
-		else 
-			print "[ERROR:] UNKNOWN DATA";
-			return;		
-		end
-	end
+    io.write(".");
+    if count >= 20 then
+        io.write("\n");
+        count = 1;
+    end
     
-	import(key,type,value);
+    count = count + 1;
+    for k,v in pairs(self) do
+        -- print(k,v)
+        if k == "key" then
+            key = v;
+        elseif k == "type" then
+            type = v;
+        elseif k == "value" then
+            value = v;
+        else
+            print "[ERROR:] UNKNOWN DATA";
+            return;
+        end
+    end
+    
+    import(key,type,value);
 end
 
 print("+++++++++++++++++++++++++++++++");
-print("++++++  IMPORT DATA start ...++");
+print("++++++ IMPORT DATA start ...++");
 print("+++++++++++++++++++++++++++++++");
 
--- init database and data file 
-local ip   = arg[2] or '127.0.0.1';
+-- init database and data file
+local ip = arg[2] or '127.0.0.1';
 local port = arg[3] or 6379;
-local which= arg[4] or 0; 
+local which= arg[4] or 0;
 local fileName = arg[1] or 'dump.rds'
 
 
@@ -116,11 +119,12 @@ redis = Redis.connect(ip,port);
 redis:select(which);
 
 dofile(fileName);
-	
+
 redis:quit();
 
 print("");
 print("+++++++++++++++++++++++++++++++");
-print("++++++++  DUMP DATA SUCCESS  ++");
+print("++++++++ DUMP DATA SUCCESS ++");
 print("+++++++++++++++++++++++++++++++");
+
 
